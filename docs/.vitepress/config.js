4ea4ead6c8db66bf76cd0b682783498c08c4b917
hello-qu <348069117@qq.com>
2025-05-12 18:04:31 +0800

    Boring optimization

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -1,7 +1,6 @@
 import { defineConfig } from 'vitepress'
 import tailwindcss from '@tailwindcss/vite'
 
-
 export default defineConfig({
   title: 'QuJun',
   description: '',
@@ -11,9 +10,20 @@ export default defineConfig({
       { text: '主页', link: '/' },
       { text: '前端', link: '/Javascript/' },
       { text: '杂记', link: '/others/' },
+      { text: '标签', link: '/tags/'},
     ]
   },
   plugins: [
     tailwindcss(),
   ],
-}) 
+  async buildEnd() {
+    // 🔥 自动创建 tags/[tag].html 路径
+    const tags = Object.keys(tagsMap)
+    this.siteConfig.pages = [
+      ...this.siteConfig.pages,
+      ...tags.map(tag => ({
+        path: `/tags/${tag}.html`
+      }))
+    ]
+  }
+})
--- docs/.vitepress/theme/index.js
+++ docs/.vitepress/theme/index.js
@@ -1,10 +1,13 @@
 import DefaultTheme from 'vitepress/theme'
 import DirIndex from './DirIndex.vue'
+import TagList from './TagList.vue'
 import './tailwind.css'
 
 export default {
   ...DefaultTheme,
   enhanceApp({ app }) {
-    app.component('DirIndex', DirIndex)
+    app
+    .component('DirIndex', DirIndex)
+    .component('TagList', TagList)
   }
 } 
